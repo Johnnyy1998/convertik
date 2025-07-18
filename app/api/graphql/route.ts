aada3182ecacdc3e2db79304dd5470ec840297c8
JanAmbroz <janambroz98@gmail.com>
2025-07-18 13:19:14 +0200

    nevim

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -60,3 +60,23 @@ const yoga = createYoga<{
 });
 
 export { yoga as GET, yoga as POST }; */
+
+import path from "path";
+import url from "url";
+import fs from "fs/promises";
+import { jsonData } from "@/utils/types";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const fileName = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(fileName);
+  const pathUrl = path.join(dirname, "..", "..", "..", "converts.json");
+
+  try {
+    const data = await fs.readFile(pathUrl, { encoding: "utf-8" });
+    const clearData: jsonData[] = await JSON.parse(data);
+    return NextResponse.json(clearData, { status: 200 });
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
